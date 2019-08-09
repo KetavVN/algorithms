afd53680647b6de0e575455205491be04163729b
Ketav <KetavVN@users.noreply.github.com>
2019-08-09 18:55:49 -0500

    ReplaceZeros

---
+++ test/src/main/java/array/ReplaceZeros.java
@@ -0,0 +1,20 @@
+//https://leetcode.com/problems/move-zeroes/
+public class ReplaceZeros {
+
+    //Runtime: 0 ms, faster than 100.00% of Java online submissions for Move Zeroes.
+    //Memory Usage: 37.7 MB, less than 100.00% of Java online submissions for Move Zeroes.
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return;
+        }
+        int ind = 0;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] != 0) {
+                nums[ind++] = nums[i];
+            }
+        }
+        while(ind < nums.length) {
+            nums[ind++] = 0;
+        }
+    }
+}
