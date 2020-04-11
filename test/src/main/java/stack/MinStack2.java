b7b36eacf8949b1771db1395ebb4701bb3eb0eae
Ketav <KetavVN@users.noreply.github.com>
2020-04-11 00:05:46 -0500

    leetcode.com/problems/min-stack

---
+++ test/src/main/java/stack/MinStack2.java
@@ -0,0 +1,64 @@
+package stack;
+
+/*
+https://leetcode.com/problems/min-stack/
+Leetcode day 10
+
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+    push(x) -- Push element x onto stack.
+    pop() -- Removes the element on top of the stack.
+    top() -- Get the top element.
+    getMin() -- Retrieve the minimum element in the stack.
+
+ 
+
+Example:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> Returns -3.
+minStack.pop();
+minStack.top();      --> Returns 0.
+minStack.getMin();   --> Returns -2.
+
+Success Details:
+5ms 68.93% faster than others
+41.2mb
+
+*/
+public class MinStack2 {
+
+    private Stack<Integer> stack = null;
+    private Stack<Integer> minStack = null;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stack.push(x);
+        int y = !minStack.isEmpty() && minStack.peek()<x ? minStack.peek() : x; 
+        minStack.push(y);
+    }
+    
+    public void pop() {
+        if(!stack.isEmpty()) {
+            stack.pop();
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return !stack.isEmpty() ? stack.peek() : 0;
+    }
+    
+    public int getMin() {
+        return !minStack.isEmpty() ? minStack.peek() : 0;
+    }
+
+}
