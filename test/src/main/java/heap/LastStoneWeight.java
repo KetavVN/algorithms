354b02a1dff3a1970c8d648c7ee15e062264a21e
Ketav <KetavVN@users.noreply.github.com>
2020-04-12 20:02:03 -0500

    leetcode.com/problems/last-stone-weight

---
+++ test/src/main/java/heap/LastStoneWeight.java
@@ -0,0 +1,54 @@
+package heap;
+
+import java.util.*;
+
+/*
+https://leetcode.com/problems/last-stone-weight
+LeetCode challenge day 12
+
+We have a collection of stones, each stone has a positive integer weight.
+
+Each turn, we choose the two heaviest stones and smash them together.  Suppose the stones have weights x and y with x <= y.  The result of this smash is:
+
+    If x == y, both stones are totally destroyed;
+    If x != y, the stone of weight x is totally destroyed, and the stone of weight y has new weight y-x.
+
+At the end, there is at most 1 stone left.  Return the weight of this stone (or 0 if there are no stones left.)
+
+Example 1:
+
+Input: [2,7,4,1,8,1]
+Output: 1
+Explanation: 
+We combine 7 and 8 to get 1 so the array converts to [2,4,1,1,1] then,
+we combine 2 and 4 to get 2 so the array converts to [2,1,1,1] then,
+we combine 2 and 1 to get 1 so the array converts to [1,1,1] then,
+we combine 1 and 1 to get 0 so the array converts to [1] then that's the value of last stone.
+ 
+Note:
+
+    1 <= stones.length <= 30
+    1 <= stones[i] <= 1000
+
+Success Details
+1ms 91.07% faster than other solutiolns
+
+*/
+public class LastStoneWeight {
+
+	public int lastStoneWeight(int[] stones) {
+        
+        Comparator<Integer> comparator = (x,y)->Integer.compare(y,x);
+        
+        PriorityQueue<Integer> queue = new PriorityQueue(comparator);
+        
+        for(int i : stones)
+            queue.add(i);
+        
+        while(queue.size() > 1)
+            queue.add(queue.poll()-queue.poll());
+        
+        return queue.poll();
+    }
+	
+}
