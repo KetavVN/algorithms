61eb0acb863c30fa322490616f5728380280b9db
Ketav <ketav@localhost.localdomain>
2021-01-23 22:12:22 -0600

    Cracking the coding interview Ch4 Problem 12 - PathSum

--- test/src/main/java/tree/PathSum.java
+++ test/src/main/java/tree/PathSum.java
@@ -1,14 +1,18 @@
 package tree;
 
-import java.util.Set;
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.ArrayList;
+import java.util.Set;
 
-/*
-find all paths in a binary tree which sum to given value.
-the path does not have to begin from root or end at leaf. But it has to be a straight line;
-*/
+/**
+ * Cracking the coding interview ch4 problem 12 - PathSum
+ * 
+ * Find all paths in a binary tree which sum to given value.
+ * The path does not have to begin from root or end at leaf. But it has to be a straight line;
+ *  
+ * @author ketav
+ */
 public class PathSum<T extends Number> {	//works only for integer for given problem
 
 	private T val;
@@ -23,46 +27,17 @@ public class PathSum<T extends Number> {	//works only for integer for given prob
 		return val.toString();
 	}
 	
-	/*private class SummaryArrayList extends ArrayList<Integer> {
-		
-		private float total;
-		
-		@Override
-		public void add(Integer ele) {	//only additions are supported for the time being
-			total+=ele;
-			super.add();
-		}
-		
-		@Override
-		public int hashCode() {
-			return currentNodeList.hashCode();
-		}
-		
-		@Override
-		public boolean equals() {
-			return currentNodeList.equals();
-		}
-		
-		public float getTotal() {
-			return total;
-		}
-		
-	}
-	
-	private void findPaths(PathSum<T> currentNode, Set<List<PathSum<T>>> list, Set<List<PathSum<T>>> temp, int val) {
-		if(currentNode == null) {
-			return;
-		}
-		
-		for(List<PathSum<T>> prevNodeList : temp) {
-			
-		}
-		
-		List<PathSum<T>> newPathFromCurrentNode = new SummaryArrayList<>();
-		newPathFromCurrentNode.add(currentNode);
-		
-	}*/
-	
+	/**
+	 * Option 1 : when traversal reaches leaf node, apply window logic
+	 * 
+	 * Cons: 
+	 * 1. too high on space complexity, 
+	 * 2. answer only available after all nodes are traversed from root to leaf (depth can be too high)
+	 * 
+	 * @param root
+	 * @param val
+	 * @return
+	 */
 	public Set<List<PathSum<T>>> findPaths(PathSum<T> root, int val) {
 		Set<List<PathSum<T>>> list = new HashSet<>(0);
 		if(root == null) {
@@ -74,7 +49,6 @@ public class PathSum<T extends Number> {	//works only for integer for given prob
 	}
 	
 	private void findPaths(PathSum<T> currentNode, Set<List<PathSum<T>>> list, List<PathSum<T>> temp, int val) {
-	
 		if(currentNode != null) {
 			temp.add(currentNode);
 			if(currentNode.left == null && currentNode.right == null) {	//reached leaf
@@ -87,21 +61,83 @@ public class PathSum<T extends Number> {	//works only for integer for given prob
 	}
 	
 	private void findSumPath(Set<List<PathSum<T>>> list, List<PathSum<T>> temp, int val) {
-		int sum = 0, startIndex=0, endIndex=0;
-		while(endIndex < temp.size()) {
-			sum += temp.get(endIndex).val.intValue();
-			endIndex++;
+		int sum = 0, windowStart=0, windowEnd=0;
+		while(windowEnd < temp.size()) {
+			sum += temp.get(windowEnd).val.intValue();
+			windowEnd++;
 			if(sum > val) {
-				sum -= temp.get(startIndex).val.intValue();
-				startIndex++;
+				sum -= temp.get(windowStart).val.intValue();
+				windowStart++;
 			} 
 			if (sum == val) {
-				int i = startIndex;
+				int i = windowStart;
 				List<PathSum<T>> lst = new ArrayList<>();
-				while(i < endIndex) {
+				while(i < windowEnd) {
 					lst.add(temp.get(i++));
 				}
 				list.add(lst);
+			}
+		}
+	}
+	
+	/**
+	 * Option 2: Maintain 2 lists: 
+	 * 		1. Running sum from each node till current node 
+	 * 			[node.value gets added to all previous items in the list
+	 * 			 current node's value alone gets appended to the list]
+	 * 		2. Running node list from root till current node
+	 * Pros: Answer available as soon as value is reached
+	 * 		Ideally Traversal can be stopped without traversing till leaf node.
+	 * 
+	 * Cons: space complexity similar to option 1 
+	 * 
+	 * @param root
+	 * @param val
+	 * @return
+	 */
+	public Set<List<PathSum<T>>> findPaths2(PathSum<T> root, int val) {
+		if(root == null) {
+			return new HashSet<>();
+		}
+
+		List<PathSum<T>> nodeList = new ArrayList<>();
+		List<Integer> sumList = new ArrayList<>();
+
+		Set<List<PathSum<T>>> ans = new HashSet<>();
+		findPaths2(root, nodeList, sumList, val, ans);
+		return ans;
+	}
+
+	private void findPaths2(PathSum<T> node, List<PathSum<T>> nodeList, 
+			List<Integer> sumList, int val, Set<List<PathSum<T>>> ans) {
+		if(node != null) {
+			nodeList = addItemToNewNodeList(nodeList, node);
+			sumList = addItemToNewSumList(sumList, node.val.intValue());
+			checkSumListMatchesToVal(nodeList, sumList, val, ans);
+			findPaths2(node.left, nodeList, sumList, val, ans);
+			findPaths2(node.right, nodeList, sumList, val, ans);
+		}
+	}
+
+	private List<PathSum<T>> addItemToNewNodeList(List<PathSum<T>> nL, PathSum<T> node) {
+		List<PathSum<T>> newList = new ArrayList<>(nL);
+		newList.add(node);
+		return newList;
+	}
+
+	private List<Integer> addItemToNewSumList(List<Integer> sumList, Integer nv) {
+		List<Integer> newList = new ArrayList<>();
+		for(int i=0; i<sumList.size(); i++) {
+			newList.add(sumList.get(i) + nv);
+		}
+		newList.add(nv);
+		return newList;
+	}
+
+	private void checkSumListMatchesToVal(List<PathSum<T>> nL, List<Integer> sL, Integer val, Set<List<PathSum<T>>> ans) {
+		for(int i=0; i<sL.size(); i++) {
+			if(sL.get(i) == val) {
+				ans.add(nL.subList(i, nL.size()));
 			}
 		}
 	}
@@ -126,7 +162,7 @@ public class PathSum<T extends Number> {	//works only for integer for given prob
 		root.right.right = new PathSum<>(9);
 		root.right.left.right = new PathSum<>(7);
 		
-		Set<List<PathSum<Integer>>> sets = root.findPaths(root, 8);
+		Set<List<PathSum<Integer>>> sets = root.findPaths2(root, 10);
 		for(List<PathSum<Integer>> lst : sets) {
 			for(PathSum<Integer> i : lst) {
 				System.out.print(i.val.intValue()+" ");
